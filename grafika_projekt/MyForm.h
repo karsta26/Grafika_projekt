840b9fa4a0b930f642d0e6ea8840c1e725e988dd
Karol Stasiak <karsta26@gmail.com>
2017-06-10 20:47:23 +0200

    Dodane wyświetlanie obrazka i zapis do pliku.

--- grafika_projekt/MyForm.h
+++ grafika_projekt/MyForm.h
@@ -30,6 +30,10 @@ namespace grafika_projekt {
 			//
 			this->label3->Text = "0";
 			this->label4->Text = "1-8";
+			openFileDialog1->Filter = "Tiff Files|*.tif|Tiff Files|*.tiff";
+			openFileDialog1->Title = "Select a Tiff File";
+			saveFileDialog1->Filter = "Bmp Files|*.bmp";
+			saveFileDialog1->Title = "Select a bmp File";
 		}
 
 	protected:
@@ -41,12 +45,19 @@ namespace grafika_projekt {
 			if (components)
 			{
 				delete components;
+				if (originImage)
+					delete originImage;
+				if (currentImage)
+					delete currentImage;
 			}
+			
 		}
 	private: System::Windows::Forms::Button^  button1;
 	protected:
 		System::String^ name;
 		CImg<myType>* originImage;
+		CImg<myType>* currentImage;
+		Bitmap^ image1;
 		
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 	private: System::Windows::Forms::Button^  button2;
@@ -59,6 +70,12 @@ namespace grafika_projekt {
 	private: System::Windows::Forms::Button^  button5;
 	private: System::Windows::Forms::Label^  label3;
 	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::Panel^  panel2;
+	private: System::Windows::Forms::Panel^  panel3;
+	private: System::Windows::Forms::SaveFileDialog^  saveFileDialog1;
+
 
 
 	private: System::ComponentModel::IContainer^  components;
@@ -89,11 +106,20 @@ namespace grafika_projekt {
 			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
+			this->panel3->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(36, 12);
+			this->button1->Location = System::Drawing::Point(36, 28);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
@@ -107,7 +133,7 @@ namespace grafika_projekt {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(12, 58);
+			this->button2->Location = System::Drawing::Point(18, 57);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(119, 23);
 			this->button2->TabIndex = 1;
@@ -117,7 +143,7 @@ namespace grafika_projekt {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(12, 99);
+			this->button3->Location = System::Drawing::Point(18, 93);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(119, 23);
 			this->button3->TabIndex = 2;
@@ -127,7 +153,7 @@ namespace grafika_projekt {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(12, 190);
+			this->button4->Location = System::Drawing::Point(18, 219);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(119, 22);
 			this->button4->TabIndex = 3;
@@ -138,7 +164,7 @@ namespace grafika_projekt {
 			// hScrollBar1
 			// 
 			this->hScrollBar1->LargeChange = 9;
-			this->hScrollBar1->Location = System::Drawing::Point(12, 253);
+			this->hScrollBar1->Location = System::Drawing::Point(18, 304);
 			this->hScrollBar1->Maximum = 17;
 			this->hScrollBar1->Minimum = 1;
 			this->hScrollBar1->Name = L"hScrollBar1";
@@ -150,7 +176,7 @@ namespace grafika_projekt {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(12, 125);
+			this->label1->Location = System::Drawing::Point(33, 128);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(104, 13);
 			this->label1->TabIndex = 5;
@@ -159,17 +185,16 @@ namespace grafika_projekt {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(13, 215);
+			this->label2->Location = System::Drawing::Point(19, 254);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(118, 13);
 			this->label2->TabIndex = 6;
 			this->label2->Text = L"wybierz 8-bitowy zakres";
-			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
 			// 
 			// hScrollBar2
 			// 
 			this->hScrollBar2->LargeChange = 1;
-			this->hScrollBar2->Location = System::Drawing::Point(9, 161);
+			this->hScrollBar2->Location = System::Drawing::Point(15, 181);
 			this->hScrollBar2->Maximum = 30;
 			this->hScrollBar2->Name = L"hScrollBar2";
 			this->hScrollBar2->Size = System::Drawing::Size(122, 17);
@@ -178,7 +203,7 @@ namespace grafika_projekt {
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(36, 294);
+			this->button5->Location = System::Drawing::Point(36, 347);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(78, 23);
 			this->button5->TabIndex = 8;
@@ -189,7 +214,7 @@ namespace grafika_projekt {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(60, 148);
+			this->label3->Location = System::Drawing::Point(76, 150);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(0, 13);
 			this->label3->TabIndex = 9;
@@ -197,37 +222,88 @@ namespace grafika_projekt {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(60, 240);
+			this->label4->Location = System::Drawing::Point(76, 280);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(0, 13);
 			this->label4->TabIndex = 10;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->pictureBox1->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pictureBox1->Location = System::Drawing::Point(0, 0);
+			this->pictureBox1->MinimumSize = System::Drawing::Size(450, 400);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(504, 411);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 11;
+			this->pictureBox1->TabStop = false;
+			// 
+			// panel1
+			// 
+			this->panel1->AutoSize = true;
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel1->Location = System::Drawing::Point(158, 0);
+			this->panel1->MinimumSize = System::Drawing::Size(300, 300);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(504, 411);
+			this->panel1->TabIndex = 12;
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->button1);
+			this->panel2->Controls->Add(this->button2);
+			this->panel2->Controls->Add(this->label4);
+			this->panel2->Controls->Add(this->button3);
+			this->panel2->Controls->Add(this->label3);
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->button5);
+			this->panel2->Controls->Add(this->hScrollBar2);
+			this->panel2->Controls->Add(this->hScrollBar1);
+			this->panel2->Controls->Add(this->label2);
+			this->panel2->Controls->Add(this->button4);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Left;
+			this->panel2->Location = System::Drawing::Point(0, 0);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(158, 411);
+			this->panel2->TabIndex = 13;
+			// 
+			// panel3
+			// 
+			this->panel3->Controls->Add(this->panel1);
+			this->panel3->Controls->Add(this->panel2);
+			this->panel3->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel3->Location = System::Drawing::Point(0, 0);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(662, 411);
+			this->panel3->TabIndex = 14;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(340, 338);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->button5);
-			this->Controls->Add(this->hScrollBar2);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->hScrollBar1);
-			this->Controls->Add(this->button4);
-			this->Controls->Add(this->button3);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
+			this->AutoScroll = true;
+			this->AutoSize = true;
+			this->ClientSize = System::Drawing::Size(662, 411);
+			this->Controls->Add(this->panel3);
+			this->MinimumSize = System::Drawing::Size(650, 450);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Obraz 16 bitowy";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			this->panel3->ResumeLayout(false);
+			this->panel3->PerformLayout();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-		openFileDialog1->Filter = "Tiff Files|*.tif|Tiff Files|*.tiff";
-		openFileDialog1->Title = "Select a Tiff File";
+		if (originImage)
+			delete originImage;
 
 		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
 		{
@@ -236,19 +312,18 @@ namespace grafika_projekt {
 			const char* chars = (const char*)(Marshal::StringToHGlobalAnsi(name)).ToPointer();
 			originImage = new CImg<myType>();
 			originImage->load_tiff(chars);
-			
-
-			CImg<myType> out1 = Transform::LinearTo8Bit(*originImage);
-			CImg<myType> out2 = Transform::BitPerBit(*originImage,3,11);
-			CImg<myType> out3 = Transform::GammaCurve(*originImage, 1.7);
-			(out1, out2, out3).display();	
+			Marshal::FreeHGlobal(IntPtr((void*)chars));
+			MessageBox::Show("Wczytano obrazek!");
+		}
+		else
+		{
+			MessageBox::Show("Wysp¹pi³ b³¹d!");
 		}
 	}
 	private: System::Void hScrollBar1_Scroll(System::Object^  sender, System::Windows::Forms::ScrollEventArgs^  e) {
 		this->label4->Text = System::String::Concat(System::Convert::ToString(e->NewValue), "-", System::Convert::ToString(e->NewValue + 7));
 	}
-	private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
-	}
+
 	private: System::Void hScrollBar2_Scroll(System::Object^  sender, System::Windows::Forms::ScrollEventArgs^  e) {
 		this->label3->Text = System::Convert::ToString(System::Convert::ToDouble(e->NewValue)/10.0);
 	}
@@ -256,21 +331,87 @@ namespace grafika_projekt {
 		//
 		//tutaj wywolac funkcje wyswietlajaca obraz dla kompresji liniowej
 		//
+		if (!originImage)
+		{
+			MessageBox::Show("Najpierw stwórz obrazek");
+			return;
+		}
+		CImg<myType> img = Transform::LinearTo8Bit(*originImage);
+		if (currentImage)
+			delete currentImage;
+		currentImage = new CImg<myType>(img);
+		changeDisplayImage(img);
 	}
 	private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
 		//
 		//tutaj pokazujemy obraz na podstawie krzywej gamma
+		//
+		if (!originImage)
+		{
+			MessageBox::Show("Najpierw stwórz obrazek");
+			return;
+		}
 		double gamma = this->hScrollBar2->Value;
+		CImg<myType> img = Transform::GammaCurve(*originImage, gamma);
+		if (currentImage)
+			delete currentImage;
+		currentImage = new CImg<myType>(img);
+		changeDisplayImage(img);
 	}
 	private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
 		//
 		//tutaj wycinamy 8-bitowy zakres
 		//
+		if (!originImage)
+		{
+			MessageBox::Show("Najpierw stwórz obrazek");
+			return;
+		}
 		int from = this->hScrollBar1->Value;
+		CImg<myType> img = Transform::BitPerBit(*originImage, from, from+8);
+		if (currentImage)
+			delete currentImage;
+		currentImage = new CImg<myType>(img);
+		changeDisplayImage(img);
 	}
 	private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
 		//
 		//zapisywanie obrazu do pliku
+		//
+		if (!currentImage)
+		{
+			MessageBox::Show("Najpierw stwórz obrazek");
+			return;
+		}
+			
+		if (saveFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+		{
+			name = (gcnew System::String(saveFileDialog1->FileName));
+			using namespace Runtime::InteropServices;
+			const char* chars = (const char*)(Marshal::StringToHGlobalAnsi(name)).ToPointer();
+			currentImage->save(chars);
+			Marshal::FreeHGlobal(IntPtr((void*)chars));
+			MessageBox::Show("Obrazek zapisano!");
+		}
+		else
+		{
+			MessageBox::Show("Wyst¹pi³ b³¹d!");
+		}
+	}
+	private: System::Void changeDisplayImage(const CImg<myType>& img) {
+		image1 = gcnew Bitmap(img.width(), img.height());
+
+		cimg_forXY(img, x, y)
+		{
+			int r = static_cast<int>(img(x, y, 0));
+			int g = static_cast<int>(img(x, y, 1));
+			int b = static_cast<int>(img(x, y, 2));
+
+			Color pixelColor = Color::FromArgb(r, g, b);
+			image1->SetPixel(x, y, pixelColor);
+		}
+
+		pictureBox1->Image = image1;
 	}
 };
 }
