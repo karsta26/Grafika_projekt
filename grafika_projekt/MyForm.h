66631c7244a7d4a7d30273aadee613630d028429
pawel <p.m.r@gmail.com>
2017-06-10 17:58:46 +0200

    interfejs graficzny

--- grafika_projekt/MyForm.h
+++ grafika_projekt/MyForm.h
@@ -28,6 +28,8 @@ namespace grafika_projekt {
 			//
 			//TODO: Add the constructor code here
 			//
+			this->label3->Text = "0";
+			this->label4->Text = "1-8";
 		}
 
 	protected:
@@ -47,13 +49,26 @@ namespace grafika_projekt {
 		CImg<myType>* originImage;
 		
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::HScrollBar^  hScrollBar1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::HScrollBar^  hScrollBar2;
+	private: System::Windows::Forms::Button^  button5;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+
+
+	private: System::ComponentModel::IContainer^  components;
 	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -64,11 +79,21 @@ namespace grafika_projekt {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->hScrollBar1 = (gcnew System::Windows::Forms::HScrollBar());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->hScrollBar2 = (gcnew System::Windows::Forms::HScrollBar());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(98, 108);
+			this->button1->Location = System::Drawing::Point(36, 12);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
@@ -80,15 +105,123 @@ namespace grafika_projekt {
 			// 
 			this->openFileDialog1->FileName = L"openFileDialog1";
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 58);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(119, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"kompresja liniowa";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(12, 99);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(119, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"krzywa gamma";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(12, 190);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(119, 22);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"bez kompresji";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// hScrollBar1
+			// 
+			this->hScrollBar1->LargeChange = 9;
+			this->hScrollBar1->Location = System::Drawing::Point(12, 253);
+			this->hScrollBar1->Maximum = 17;
+			this->hScrollBar1->Minimum = 1;
+			this->hScrollBar1->Name = L"hScrollBar1";
+			this->hScrollBar1->Size = System::Drawing::Size(119, 17);
+			this->hScrollBar1->TabIndex = 4;
+			this->hScrollBar1->Value = 1;
+			this->hScrollBar1->Scroll += gcnew System::Windows::Forms::ScrollEventHandler(this, &MyForm::hScrollBar1_Scroll);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 125);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(104, 13);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"wybierz wsp. gamma";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(13, 215);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(118, 13);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"wybierz 8-bitowy zakres";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// hScrollBar2
+			// 
+			this->hScrollBar2->LargeChange = 1;
+			this->hScrollBar2->Location = System::Drawing::Point(9, 161);
+			this->hScrollBar2->Maximum = 30;
+			this->hScrollBar2->Name = L"hScrollBar2";
+			this->hScrollBar2->Size = System::Drawing::Size(122, 17);
+			this->hScrollBar2->TabIndex = 7;
+			this->hScrollBar2->Scroll += gcnew System::Windows::Forms::ScrollEventHandler(this, &MyForm::hScrollBar2_Scroll);
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(36, 294);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(78, 23);
+			this->button5->TabIndex = 8;
+			this->button5->Text = L"zapisz obraz";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(60, 148);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(0, 13);
+			this->label3->TabIndex = 9;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(60, 240);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(0, 13);
+			this->label4->TabIndex = 10;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(340, 338);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->hScrollBar2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->hScrollBar1);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -111,5 +244,33 @@ namespace grafika_projekt {
 			(out1, out2, out3).display();	
 		}
 	}
-	};
+	private: System::Void hScrollBar1_Scroll(System::Object^  sender, System::Windows::Forms::ScrollEventArgs^  e) {
+		this->label4->Text = System::String::Concat(System::Convert::ToString(e->NewValue), "-", System::Convert::ToString(e->NewValue + 7));
+	}
+	private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void hScrollBar2_Scroll(System::Object^  sender, System::Windows::Forms::ScrollEventArgs^  e) {
+		this->label3->Text = System::Convert::ToString(System::Convert::ToDouble(e->NewValue)/10.0);
+	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		//
+		//tutaj wywolac funkcje wyswietlajaca obraz dla kompresji liniowej
+		//
+	}
+	private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+		//
+		//tutaj pokazujemy obraz na podstawie krzywej gamma
+		double gamma = this->hScrollBar2->Value;
+	}
+	private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
+		//
+		//tutaj wycinamy 8-bitowy zakres
+		//
+		int from = this->hScrollBar1->Value;
+	}
+	private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
+		//
+		//zapisywanie obrazu do pliku
+	}
+};
 }
